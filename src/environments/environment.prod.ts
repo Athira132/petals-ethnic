f0c09c56b2ab578470e7388c0ff568b3035929f7
Athira132 <athiraanuss12@gmail.com>
2026-08-18 14:18:59 +0530

    Fix environment.ts and environment.prod.ts publishable key mismatch regression

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   supabaseUrl: 'https://dmpltyqedymhggdtexto.supabase.co',
-  supabaseKey: 'sb_publishable_JHt31eRCBafVoRI-_LKswA_LZfAjyYr',
+  supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRtcGx0eXFlZHltaGdnZHRleHRvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODY3ODEzNzYsImV4cCI6MjEwMjM1NzM3Nn0.GvioERQdSKhoJEPj3-6WiOqCqaXDTGVtgDkvsVjnulk',
   razorpayKeyId: 'rzp_test_5113899319'
 };
